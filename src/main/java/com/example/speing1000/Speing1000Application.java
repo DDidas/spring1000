34e963536998418a2abb893bec95386a53fb0ed6
DDidas <101012907+DDidas@users.noreply.github.com>
2023-06-20 13:22:56 +0200

    Initial commit

---
+++ src/main/java/com/example/speing1000/Speing1000Application.java
@@ -0,0 +1,13 @@
+package com.example.speing1000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Speing1000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Speing1000Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/speing1000/Speing1000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speing1000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Speing1000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
